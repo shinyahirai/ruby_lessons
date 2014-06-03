e0fcabda77b89de42a7aadd411031ee9542422bf
ShinyaHirai <908.shinya@gmail.com>
2014-06-03 16:05:27 +0900

    Finished lesson 17.

--- hello.rb
+++ hello.rb
@@ -309,3 +309,35 @@ end
 end
 
 
+# レッスン 17
+
+# 繰り返し処理
+# for
+# each
+for i in 0..2 do # 0から2までの数字を表現
+	puts i
+end
+
+for color in ["red", "blue", "pink"] do 
+	puts color
+end
+
+["red", "blue", "pink"].each do |color| # 要素の数分繰り返す
+	puts color
+end
+
+{"red" => 200, "blue" => 100, "pink" => 50}.each do |color, price| # ハッシュにも使うことができる
+	puts "#{color}: #{price}"
+end
+
+
+
+
+
+
+
+
+
+
+
+
