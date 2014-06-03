ba4712f08741dda68d2dd8e1a37d99223bc35f85
ShinyaHirai <908.shinya@gmail.com>
2014-06-03 15:59:13 +0900

    Finished lesson 16.

--- hello.rb
+++ hello.rb
@@ -274,10 +274,38 @@ when "yellow"
 else
 	puts "wrong signal"
 end
-	
+
 			
+# レッスン 16
 
+# 繰り返し処理
+# times
+# while
+3.times do |i| # ３回繰り返す iには何回目かが入っている
+	puts "#{i}: hello" # 変数展開で使うことができる
+end
 
+i = 0
+while i < 3 do
+	puts "#{i}: hello"
+	i += 1
+end
 
+# break: ループを抜ける
+# next: ループを一回スキップ
+
+3.times do |i|
+	if i == 1
+		break
+	end
+	puts "#{i}: hello"
+end
+
+3.times do |i|
+	if i == 1
+		next
+	end
+	puts "#{i}: hello"
+end
 
 
